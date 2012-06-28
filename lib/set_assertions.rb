3a0f8a2674e1aa9bf5046dc152c9ed059c0307be
Rasheed Abdul-Aziz <squeedee@gmail.com>
2012-06-28 23:01:52 +1200

    Added negated assertion. Added documentation

--- lib/set_assertions.rb
+++ lib/set_assertions.rb
@@ -2,13 +2,41 @@
 require "set_assertions/version"
 
 module SetAssertions
+
+  ##
+  # Passes if +expected_set+ and +actual_set+ are equal sets.
+  # An equal set has the following properties.
+  #   * Identical cardinality.
+  #   * The same elements, without concern for order.
+  #
+  # Example:
+  #   assert_equal_set [:a,:b], [:b,:a]
+
   def assert_equal_set(expected, actual, message=nil)
-    full_message = build_message(message, "array with <?> in any order was expected, got: \n<?>.\n", expected, actual)
+    full_message = build_message(message, "set <?> in any order was expected, got: \n<?>.\n", expected, actual)
     assert_block(full_message) {
       (expected.length == actual.length) &&
         ((expected & actual).length == actual.length)
     }
   end
+
+  ##
+  # Passes if +expected_set+ and +actual_set+ are inequal sets.
+  # An inequal set has the following properties.
+  #   * Different cardinality.
+  #   * The same cardinality, but different elements. (order does not matter)
+  #
+  # Example:
+  #   assert_not_equal_set [:a,:b], [:a,:c]
+
+  def assert_not_equal_set(expected, actual, message=nil)
+    full_message = build_message(message, "any set other than <?> was expected, got the same: \n<?>.\n", expected, actual)
+    assert_block(full_message) {
+      (expected.length != actual.length) ||
+        ((expected & actual).length != actual.length)
+    }
+  end
+
 end
 
 class Test::Unit::TestCase
--- test/test_set_assertions.rb
+++ test/test_set_assertions.rb
@@ -4,34 +4,62 @@ require 'test/unit'
 require 'set_assertions'
 
 class SetAssertionsTest < Test::Unit::TestCase
-  def test_two_empty_tests_pass
-    assert_equal_set([], [])
+
+  class EqualSetTest < Test::Unit::TestCase
+    def test_two_empty_tests_pass
+      assert_equal_set([], [])
+    end
+
+    def test_two_identical_unit_sets_pass
+      assert_equal_set([:a], [:a])
+    end
+
+    def test_an_empty_set_and_a_unit_set_fail
+      assert_fail_assertion { assert_equal_set([:a], []) }
+      assert_fail_assertion { assert_equal_set([], [:a]) }
+    end
+
+    def test_order_does_not_matter
+      assert_equal_set([:a, :b], [:b, :a])
+    end
+
+    def test_same_cardinality_but_different_set_fails
+      assert_fail_assertion { assert_equal_set([:a, :b], [:c, :b]) }
+    end
+
+    def test_large_cardinality_passes
+      assert_equal_set([:a, :b, "c", 4, "f", :g, :h], [:a, :b, "c", 4, "f", :g, :h])
+    end
+
+    def test_object_equality_passes
+      a = "foo"
+      b = {:foo => "bar"}
+      assert_equal_set([a, b], [b, a])
+    end
   end
 
-  def test_two_identical_unit_sets_pass
-    assert_equal_set([:a], [:a])
-  end
+  class NotEqualSetTest < Test::Unit::TestCase
+    def test_two_empty_tests_fail
+      assert_fail_assertion { assert_not_equal_set([], []) }
+    end
 
-  def test_an_empty_set_and_a_unit_set_fail
-    assert_fail_assertion { assert_equal_set([:a], []) }
-    assert_fail_assertion { assert_equal_set([], [:a]) }
-  end
+    def test_two_identical_unit_sets_fail
+      assert_fail_assertion { assert_not_equal_set([:a], [:a]) }
+    end
 
-  def test_order_does_not_matter
-    assert_equal_set([:a, :b], [:b, :a])
-  end
+    def test_an_empty_set_and_a_unit_set_pass
+      assert_not_equal_set([:a], [])
+      assert_not_equal_set([], [:a])
+    end
 
-  def test_same_cardinality_but_different_set_fails
-    assert_fail_assertion { assert_equal_set([:a, :b], [:c, :b]) }
-  end
+    def test_same_cardinality_but_different_set_passes
+      assert_not_equal_set([:a, :b], [:c, :b])
+    end
 
-  def test_large_cardinality_passes
-    assert_equal_set([:a, :b, "c", 4, "f", :g, :h], [:a, :b, "c", 4, "f", :g, :h])
-  end
-
-  def test_object_equality_passes
-    a = "foo"
-    b = {:foo => "bar"}
-    assert_equal_set([a,b], [b,a])
+    def test_object_equality_fails
+      a = "foo"
+      b = {:foo => "bar"}
+      assert_fail_assertion { assert_not_equal_set([a, b], [b, a]) }
+    end
   end
 end
