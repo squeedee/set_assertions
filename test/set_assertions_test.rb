e56eea7e75ef226cef3a6ce3a000f9fe577e82bf
Rasheed Abdul-Aziz <squeedee@gmail.com>
2012-06-28 14:44:34 +1200

    Fixed tests, all working and showing at least some reliability in the gem

--- test/set_assertions_test.rb
+++ test/set_assertions_test.rb
@@ -5,15 +5,33 @@ require 'set_assertions'
 
 class SetAssertionsTest < Test::Unit::TestCase
   def test_two_empty_tests_pass
-    assert_identical_set([],[])
+    assert_identical_set([], [])
   end
 
   def test_two_identical_unit_sets_pass
-    assert_identical_set([:a],[:a])
+    assert_identical_set([:a], [:a])
   end
 
   def test_an_empty_set_and_a_unit_set_fail
-    assert_identical_set([:a],[])
+    assert_fail_assertion { assert_identical_set([:a], []) }
+    assert_fail_assertion { assert_identical_set([], [:a]) }
   end
 
+  def test_order_does_not_matter
+    assert_identical_set([:a, :b], [:b, :a])
+  end
+
+  def test_same_cardinality_but_different_set_fails
+    assert_fail_assertion { assert_identical_set([:a, :b], [:c, :b]) }
+  end
+
+  def test_large_cardinality_passes
+    assert_identical_set([:a, :b, "c", 4, "f", :g, :h], [:a, :b, "c", 4, "f", :g, :h])
+  end
+
+  def test_object_equality_passes
+    a = "foo"
+    b = {:foo => "bar"}
+    assert_identical_set([a,b], [b,a])
+  end
 end
